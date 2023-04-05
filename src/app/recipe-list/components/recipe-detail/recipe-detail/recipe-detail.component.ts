7497e67091e3b3bea59062a3083551e87eb0d10a
GustavNordgren95 <gustav_nordgren@outlook.com>
2023-04-06 00:05:14 +0200

    Lagt till detaljsida Separerat kod i komponenter

--- src/app/recipe-list/components/recipe-detail/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-list/components/recipe-detail/recipe-detail/recipe-detail.component.ts
@@ -7,24 +7,14 @@ import { RecipesList } from 'src/app/recipes';
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent {
-  
+
   @Input()
   recipe?: RecipesList;
 
   @Output()
   close = new EventEmitter<boolean>();
   
-  email?: string;
-  showForm: boolean = true;
-  showReceipt: boolean = false;
-
   onCloseConfigurator(sentRequest: boolean) {
     this.close.emit(sentRequest);
-  }
-  
-  onSubmit() {
-    // Registrera kundens e-post genom att anropa något API...
-    this.showForm = false;
-    this.showReceipt = true;
   }
 }
